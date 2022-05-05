e905fac877fafa04da07885d90d7105da343d738
jsb100800@naver.com <jsb100800@naver.com>
2022-05-05 23:30:19 +0900

    [UPDATE] chat slide focus view

--- src/main/java/com/example/interback_be/utils/Constants.java
+++ src/main/java/com/example/interback_be/utils/Constants.java
@@ -1,6 +1,6 @@
 package com.example.interback_be.utils;
 
 public class Constants {
-    public static final String API_URL = "https://52.78.40.40:80";
-    //public static final String API_URL = "http://localhost:8000";
+    //public static final String API_URL = "https://52.78.40.40:80";
+    public static final String API_URL = "http://localhost:8000";
 }
